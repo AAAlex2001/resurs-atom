7449c3116714d431ca5ae08be389f9cf7d5717f6
AAAlex2001 <sasha_tolstov_2001@mail.ru>
2026-06-16 16:20:16 +0300

    hero

--- frontend/src/widgets/Hero/index.tsx
+++ frontend/src/widgets/Hero/index.tsx
@@ -2,28 +2,44 @@ import style from "./style.module.scss";
 import { Button } from "@/shared/ui/Button";
 
 type HeroProps ={
-    title?: string;
+    imageAlt?: string;
     subtitle?: string;
     image?: string;
     buttonOutlineText?: string;
     buttonFilledText?: string;
+    hilightText?: string;
+    titleSuffix?: string;
+    titlePrefix?: string;
 }
 
 export const Hero = (
     {
-        title,
+        imageAlt,
         subtitle,
         image,
         buttonOutlineText,
         buttonFilledText,
+        hilightText,
+        titleSuffix,
+        titlePrefix,
     }: HeroProps) => {
         return (
             <div className={style.hero}>
-                <h1 className={style.title}>{title}</h1>
-                <p className={style.subtitle}>{subtitle}</p>
-                <img src={image} alt={title} className={style.image} />
-                <Button text={buttonOutlineText} variant="outline" />
-                <Button text={buttonFilledText} variant="filled" />
+                <div className={style.heroContent}>
+                    <div className={style.heroTexts}>
+                        <h1 className={style.title}>
+                            {titlePrefix}
+                            <span className={style.hilightText}>{hilightText}</span> 
+                            {titleSuffix}
+                        </h1>
+                        <p className={style.subtitle}>{subtitle}</p>
+                    </div>
+                    <div className={style.heroButtons}>
+                        <Button text={buttonFilledText} variant="filled" />
+                        <Button text={buttonOutlineText} variant="outline" />
+                    </div>
+                </div>
+                <img src={image} alt={imageAlt} className={style.image} />
             </div>
         )
     };
