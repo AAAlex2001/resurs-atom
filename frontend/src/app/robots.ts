e7268a6b907bede1b591709fa53c56691327abe3
AAAlex2001 <sasha_tolstov_2001@mail.ru>
2026-06-18 15:58:20 +0300

    правка

--- frontend/src/app/robots.ts
+++ frontend/src/app/robots.ts
@@ -2,10 +2,13 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
     return {
-        rules: {
-            userAgent: "*",
-            allow: "/",
-        },
+        rules: [
+            {
+                userAgent: "*",
+                allow: "/",
+                disallow: "/f7k2x9n3/",
+            },
+        ],
         sitemap: "https://atom-plus.pro/sitemap.xml",
         host: "https://atom-plus.pro",
     };
